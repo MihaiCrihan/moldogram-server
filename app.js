0ed366f41fb3dd0e36e9c4b3cf3507abc692e9a8
Mihai <haos-sah@mail.ru>
2022-04-22 10:02:31 +0300

    fix patch and add for message api

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require('cors');
 
 var indexRouter = require("./routes");
 var postsRouter = require("./routes/posts");
+var messagesRouter = require("./routes/messages");
 
 var app = express();
-
 app.use(logger("dev"));
+
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  console.log(req);
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -31,5 +35,6 @@ app.use((req, res, next) => {
 
 app.use("/", indexRouter);
 app.use("/posts", postsRouter);
+app.use("/messages", messagesRouter);
 
 module.exports = app;
--- routes/messages.js
+++ routes/messages.js
@@ -7,29 +7,7 @@ const items = Array(10).fill(0).map((_, index)=> ({
     name: "5343543",
     avatar: "https://cdn.vuetifyjs.com/images/john.jpg"
   },
-  content: "https://picsum.photos/1920",
-  description: "testsetrseresrfsd",
-  likes: 5,
-  comments: [
-    {
-      author: {
-        name: "Bumbac Marin",
-        avatar: "https://picsum.photos/200"
-      },
-      content: "Lorem ipsum stil dolore amet",
-      likes: 6,
-      created_at: "12.12.2020"
-    },
-    {
-      author: {
-        name: "Crihan Mihai",
-        avatar: "https://picsum.photos/200"
-      },
-      content: "sa presupunem ca este un comentariu",
-      likes: 23,
-      created_at: "12.12.2020"
-    }
-  ],
+  content: "some string",
   created_at: "12.12.2020"
 }));
 
